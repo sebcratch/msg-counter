5216fcc7b592a974f0ccb09cbc3c6aee409420dd
sebcratch <sebcratch@gmail.com>
2023-11-29 20:46:47 +0100

    final 1 version

--- chooseFolder.py
+++ chooseFolder.py
@@ -15,27 +15,26 @@ def autoFind():
 
     #nikomu niepotrzeby chuj
     chuj = 0
+    fb = "/your_activity_across_facebook/messages/inbox/"
 
 
     #wybór każdego zipa
-    for h in os.listdir(folder_selected):
 
         #wypakuj baranie
-        filen = h
-        with ZipFile(folder_selected+'/'+filen, "r") as zip:
-            print('Extracting all the files now...') 
-            zip.extractall() 
-            print('Done!') 
+       # filen = h
+        #with ZipFile(folder_selected+'/'+filen, "r") as zip:
+         #   print('Extracting all the files now...') 
+          #  zip.extractall() 
+           # print('Done!') 
 
 
 
-        a = os.listdir("your_activity_across_facebook/messages/inbox")
-        for i in a:
-            addDir = input(i[:i.index("_")]+" ")
-            if addDir == "y":
-                for subFile in os.listdir("your_activity_across_facebook/messages/inbox/"+i):
-                    if subFile.endswith(".json"):
-                        os.rename("your_activity_across_facebook/messages/inbox/"+i+'/'+subFile, "your_activity_across_facebook/messages/inbox/"+i+'/'+str(chuj)+".json")
-                        shutil.copy("your_activity_across_facebook/messages/inbox/"+i+'/'+str(chuj)+".json", "data")
-                        chuj+=1
-    shutil.rmtree("your_activity_across_facebook")
+    a = os.listdir(''.join([folder_selected, fb]))
+    for i in a:
+        addDir = input(i[:i.index("_")]+" ")
+        if addDir == "y":
+            for subFile in os.listdir(''.join([folder_selected, fb, i])):
+                if subFile.endswith(".json"):
+                    os.rename(''.join([folder_selected, fb, i, '/', subFile]), ''.join([folder_selected, fb, i, '/', str(chuj), ".json"]))
+                    shutil.copy(''.join([folder_selected, fb, i, '/', str(chuj), ".json"]), "data")
+                    chuj+=1
