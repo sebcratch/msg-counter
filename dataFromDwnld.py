88e8318f46e948c994a4a685e03c0fb90cd91908
sebcratch <sebcratch@gmail.com>
2023-12-05 21:23:40 +0100

    ogarnąć kolroki, wrzucić exe i jest

--- dataFromDwnld.py
+++ dataFromDwnld.py
@@ -4,8 +4,8 @@ from tkinter import *
 import time
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
-
-
+import datetime as dt
+from math import ceil
 
 def listMsgs(file):
     f = open(file, "r", encoding="raw-unicode-escape")
@@ -13,7 +13,7 @@ def listMsgs(file):
     f.close()
     outputList = []
     for i in allMsgs:
-        msgInfo = i.split(',') #rozbicie wiadomości o msg
+        msgInfo = i.split(',') #rozbicie informacji o msg
         sndrTm =(msgInfo[1][29:-1], msgInfo[2][23::]) #wziąć tylko nadawce i godzine
         pentak = sndrTm[0].encode('latin-1').decode('utf-8') #jakaś chujowa konwersja
         outputList.append((pentak, int(sndrTm[1])/1000))
@@ -71,23 +71,37 @@ def fullDataFromDwnld(mainfolder):
 
 
 
+def intervalCalc(masnySet):    
+    fDay = (int(masnySet[0 ][0]), int(masnySet[0 ][1]), int(masnySet[0 ][2]))
+    lDay = (int(masnySet[-1][0]), int(masnySet[-1][1]), int(masnySet[-1][2]))
+    fDay = time.mktime(dt.datetime(fDay[2], fDay[1], fDay[0]).timetuple())
+    lDay = time.mktime(dt.datetime(lDay[2], lDay[1], lDay[0]).timetuple())
+    return ceil((lDay - fDay)/864000)
+
+
+
 
 def simplePlotShow(masnyBen):
     for i in range(len(masnyBen)):
-        masnyBen[i] = (masnyBen[i][1][2], masnyBen[i][1][1], masnyBen[i][1][0])
-    masnySet = sorted(set(masnyBen), key=lambda x: (x[2], x[1], x[0]))
-    x,y=[],[]
+        masnyBen[i] = (str(masnyBen[i][1][2]), str(masnyBen[i][1][1]), str(masnyBen[i][1][0]))
+    masnySet = sorted(set(masnyBen), key=lambda x: (int(x[2]), int(x[1]), int(x[0])))
+    x,y=[dt.datetime.strptime('/'.join(d),'%d/%m/%Y').date() for d in masnySet],[]
     for i in masnySet:
-        dateEm = "/".join([str(v) for v in i])
-        x.append(dateEm)
         y.append(masnyBen.count(i))
-    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%d/%m/%Y'))
-    plt.gca().xaxis.set_major_locator(mdates.DayLocator(interval=10))
-    plt.plot(x, y)
-    plt.gcf().autofmt_xdate()
+
+    fig = plt.figure()
+    fig.patch.set_facecolor('#111111')
+    ax = fig.add_subplot(111)
+    ax.tick_params(axis='x', colors='#009999')
+    ax.tick_params(axis='y', colors='#009999')
+    ax.set_facecolor("#343434")
+    ax.xaxis.set_major_formatter(mdates.DateFormatter('%d/%m/%Y'))
+    ax.xaxis.set_major_locator(mdates.DayLocator(interval=intervalCalc(masnyBen)))
+    ax.plot(x, y, color="#009999")
+    
+    fig.autofmt_xdate()
     plt.show()
-    return x
 
 root = Tk()
 root.withdraw()
-print(fullDataFromDwnld(filedialog.askdirectory()))
+(fullDataFromDwnld(filedialog.askdirectory()))
