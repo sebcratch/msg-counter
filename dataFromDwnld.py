7220124d5704bb6b27f55420869d858339293639
sebcratch <sebcratch@gmail.com>
2023-12-06 22:31:08 +0100

    _

--- dataFromDwnld.py
+++ dataFromDwnld.py
@@ -56,31 +56,18 @@ def partDataFromDwnld(folder):
 
 
 
-def fullDataFromDwnld(mainfolder):
-    finalList = []
-    for subFolder in os.listdir(mainfolder):
-        importedData = partDataFromDwnld(mainfolder + '/' + subFolder)
-        finalList.extend(importedData)
-    for i in range(len(finalList)):
-        lTime = time.localtime(finalList[i][1])
-        lTime = (lTime[0],lTime[1],lTime[2],lTime[3],lTime[4],lTime[5],lTime[6])
-        finalList[i] = list((finalList[i][0], lTime))
-    finalList = sorted(finalList, key=lambda x: (x[1][0], x[1][1], x[1][2], x[1][3], x[1][4], x[1][5]))
 
-    return simplePlotShow(finalList)
-
-
-
-def intervalCalc(masnySet):    
-    fDay = (int(masnySet[0 ][0]), int(masnySet[0 ][1]), int(masnySet[0 ][2]))
+def intervalCalc(masnySet):    #liczenie interwału w wykresie
+    fDay = (int(masnySet[0 ][0]), int(masnySet[0 ][1]), int(masnySet[0 ][2])) #krotka dzień pierwszy
     lDay = (int(masnySet[-1][0]), int(masnySet[-1][1]), int(masnySet[-1][2]))
-    fDay = time.mktime(dt.datetime(fDay[2], fDay[1], fDay[0]).timetuple())
+    fDay = time.mktime(dt.datetime(fDay[2], fDay[1], fDay[0]).timetuple()) #dzień pierwszy epoch
     lDay = time.mktime(dt.datetime(lDay[2], lDay[1], lDay[0]).timetuple())
     return ceil((lDay - fDay)/864000)
 
 
 
 
+#pokazanie wykresu dziennego
 def simplePlotShow(masnyBen):
     for i in range(len(masnyBen)):
         masnyBen[i] = (str(masnyBen[i][1][2]), str(masnyBen[i][1][1]), str(masnyBen[i][1][0]))
@@ -97,11 +84,43 @@ def simplePlotShow(masnyBen):
     ax.set_facecolor("#343434")
     ax.xaxis.set_major_formatter(mdates.DateFormatter('%d/%m/%Y'))
     ax.xaxis.set_major_locator(mdates.DayLocator(interval=intervalCalc(masnyBen)))
-    ax.plot(x, y, color="#009999")
+    ax.bar(x, y, color="#009999")
     
     fig.autofmt_xdate()
     plt.show()
 
+
+
+
+def weekPlotShow(masnyBen):
+    for i in range(len(masnyBen)):
+        masnyBen[i] = (str(masnyBen[i][1][2]), str(masnyBen[i][1][1]), str(masnyBen[i][1][0]))
+    masnySet = sorted(set(masnyBen), key=lambda x: (int(x[2]), int(x[1]), int(x[0])))
+    x, y = [], []
+    for i in range(len(masnySet)//7):
+        x.append(dt.datetime.strptime('/'.join(masnySet[i]), '%d/%m/%Y').date())
+    return x
+
+
+
+
+#główna fukcja
+def fullDataFromDwnld(mainfolder):
+    finalList = []
+    for subFolder in os.listdir(mainfolder):
+        importedData = partDataFromDwnld(mainfolder + '/' + subFolder)
+        finalList.extend(importedData)
+    for i in range(len(finalList)):
+        lTime = time.localtime(finalList[i][1])#krotka z datą i badziewiami
+        lTime = (lTime[0],lTime[1],lTime[2],lTime[3],lTime[4],lTime[5],lTime[6])#krotka z datą
+        finalList[i] = list((finalList[i][0], lTime))
+    finalList = sorted(finalList, key=lambda x: (x[1][0], x[1][1], x[1][2], x[1][3], x[1][4], x[1][5]))
+
+    return simplePlotShow(finalList)
+
+
+
+
 root = Tk()
 root.withdraw()
-(fullDataFromDwnld(filedialog.askdirectory()))
+print(fullDataFromDwnld(filedialog.askdirectory()))
